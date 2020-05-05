33f45f90d42e0841058a18b48d07b9e3f118fd02
Ifedayo Fasakin <fasakin.ifedayo@gmail.com>
2020-05-05 21:51:14 +0100

    header section completed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,7 @@
-import '../styles/styles.css'
-if(module.hot) {
-    module.hot.accept()
-}
+import "../styles/styles.css";
+import MobileMenu from './modules/MobileMenu'
 
+if (module.hot) {
+  module.hot.accept();
+}
+let mobileMenu = new MobileMenu()
